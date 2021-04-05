8ca60b9541ebdd293d60d9bd0905df0a4afff408
Haytham Qushtom <haytham.qushtom@senecacollege.ca>
2021-04-05 10:23:40 -0400

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {StudentListComponent} from './student-list/student-list.component';
 import {StudentDetailComponent} from './student-detail/student-detail.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-
+    
 const routes: Routes = [
   {path:"student-list", component:StudentListComponent},
   {path:"", redirectTo: "student-list", pathMatch:"full"}, 
